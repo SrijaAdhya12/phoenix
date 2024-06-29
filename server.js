b93f54276c00a8590aa36df4bbed7139bbd43355
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-06-29 11:32:37 +0530

    fix: added PORT env variable.

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-import express from "express"
-import cors from "cors"
-const app = express()
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+const PORT = process.env.PORT
+const app = express()
 app.use(cors())
 
-app.listen(5000, () => console.log(`Server running on port ${5000}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
-app.get("/" , (_,res) => res.send("Welcome to Phoenixs"))
+app.get('/', (_, res) => res.send('Welcome to Phoenixs'))
