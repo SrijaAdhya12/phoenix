86ffe61426f15f62ae44e9ac27372b81093498fc
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-06-29 15:13:45 +0530

    fix: error message

--- controllers/posts.js
+++ controllers/posts.js
@@ -20,6 +20,6 @@ export const getPosts = (req, res) => {
         res.status(200).json(posts)
         
     } catch (error) {
-       res.status(500).json(posts)
+       res.status(500).json({ error: error.message })
     }
 }
