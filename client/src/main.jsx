80a398d55572524a36a07f5c9ce0d9b07ea980a6
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-07-21 17:17:03 +0530

    feat: private and auth routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,12 @@
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
+	<StrictMode>
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
-	</React.StrictMode>
+	</StrictMode>
 )
